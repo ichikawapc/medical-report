2ebffe942f8de1d86d76c2ac9faeef453383cea4
makkuroshippo <makkuro.s.5526@gmail.com>
2024-11-25 23:46:17 +0900

    add commands of transfar page

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
-function onSubmit(event) {
-    alert("hello");
-}
-
-let button = document.getElementById("submit");
-button.onclick = onSubmit;
+// let button = document.getElementById("submit");
+// button.onclick = onSubmit;
 
 function confirmFormC() {
+    confirmId.innerText = id.value;
 }
 
+let id = document.getElementById("id");
+let confirmId = document.getElementById("confirmId");
+
+// 戻るボタンの操作
 function back() {
-    return window.confirm("報告書作成を終了します、");
+    window.location.href = "carotid.html";
 }
+
+// 終了ボタンの操作
+function fin() {
+    if (window.confirm("報告書作成を終了します。")) {
+        // index.htmlへ戻る
+        window.location.href = "index.html";
+    } else {
+        // 現ページへ留まる(何もしない)
+    }
+}
+
+document.getElementById("id").textContent = id;
